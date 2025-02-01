44910dd0585fd30b76b688851052f23f4e72114a
bluetopazz <ronniemacketc@gmail.com>
2025-02-01 19:21:08 +0100

    added graph image and meta tags

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -9,7 +9,29 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <link rel="icon" href="/images/icon.svg" />
+        {/* Favicon */}
+        <link rel="icon" href="/images/mirror.svg" />
+
+        {/* Open Graph / Social Sharing Meta Tags */}
+        <meta property="og:title" content="Mirror Progress" />
+        <meta
+          property="og:description"
+          content="Mirror Progress provides expert consulting and digital transformation services to help organizations innovate and thrive. Learn how our experienced team can drive your business forward."
+        />
+        <meta property="og:image" content="/images/graphimage.png" />
+        <meta property="og:url" content="https://mirrorprogress.com" />
+        <meta property="og:type" content="website" />
+        <meta property="og:site_name" content="Mirror Progress" />
+        <meta property="og:locale" content="en_US" />
+
+        {/* Twitter Card meta tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Mirror Progress" />
+        <meta
+          name="twitter:description"
+          content="Mirror Progress provides expert consulting and digital transformation services to help organizations innovate and thrive. Learn how our experienced team can drive your business forward."
+        />
+        <meta name="twitter:image" content="/images/graphimage.png" />
       </Head>
       <Layout>
         <Component {...pageProps} />
